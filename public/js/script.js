d0b52753182a341aa69d910f0565961989bb54c9
Loris <loris4526@gmail.com>
2017-08-28 17:34:31 +0200

    Create 'home' view that renders created template. Append it to body via jQuery

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,22 @@
-console.log('script is here!!');
+//set up Handlebars
+Handlebars.templates = Handlebars.templates || {};
+const templates = document.querySelectorAll('template');
+Array.prototype.slice.call(templates).forEach(function(tmpl) {
+    Handlebars.templates[tmpl.id] = Handlebars.compile(tmpl.innerHTML.replace(/{{&gt;/g, '{{>'));
+});
+
+//create View for home page
+const HomeView = Backbone.View.extend({
+  initialize: function(){},
+  render: function(){
+    const html = Handlebars.templates.home();;
+    this.$el.html(html);
+  }
+});
+const home = new HomeView({
+  el: '#main',
+  model: {}
+});
+
+//render homepage to body
+$('body').append(home.render());
