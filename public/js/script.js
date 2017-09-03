869552af5239adc0ef1ac6e94cf906308a4b6be0
Loris <loris4526@gmail.com>
2017-09-03 12:05:08 +0200

    Re-arrange and comment out main 'script.js' file

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,37 @@
-//setup variables (must be equal on server-side)
-//set number of images you wanna first load on 'HomeView' and number of comments you wanna first load on 'ImageView'
+//set number of images you wanna first load on 'HomeView' and number of comments you wanna first load on 'ImageView'.
+//PS. Those variables must be equal server-side (for pagination)
 const imagesLoaded = 6;
 const commentsLoaded = 3;
 
+//set up Handlebars
+Handlebars.templates = Handlebars.templates || {};
+const templates = document.querySelectorAll('template');
+Array.prototype.slice.call(templates).forEach(function(tmpl) {
+  Handlebars.templates[tmpl.id] = Handlebars.compile(tmpl.innerHTML.replace(/{{&gt;/g, '{{>'));
+});
+
+//override built-in Backbone behaviour for Views --> remove previous event handlers on current View before mounting a new one
+const oldSetElement = Backbone.View.prototype.setElement;
+Backbone.View.prototype.setElement = function(el){
+  $(el).off();
+  oldSetElement.call(this,el);
+};
+
+
+//implement infinite scrolling
+(function myTimer(){
+  searchTimeout = setTimeout(function(){
+    if($(document).scrollTop() + $(window).height() > $(document).height()-50){
+      //when reaching bottom page, ask models for new data
+      $('#more-images').trigger('loadImages');
+      $('#more-comments').trigger('loadComments');
+    }
+    // loop over the timer again
+    myTimer();
+  },1000)
+}());
+
+//overall general-purpose functions
 function addOverlay(){
   $('.overlay').show();
   $('body').addClass('prevent-scrolling');
@@ -12,42 +41,8 @@ function removeOverlay(){
   $('body').removeClass('prevent-scrolling');
 }
 
-//implement infinite scrolling
-(function myTimer(){
-  searchTimeout = setTimeout(function(){
-    if($(document).scrollTop() + $(window).height() > $(document).height()-50){
-      // if we're on bottom, and there are more results to be displayed, get them
-      $('#more-images').trigger('loadImages');
-      $('#more-comments').trigger('loadComments');
-    }
-    // loop over the timer again
-    myTimer();
-  },1000)
-}());
 
 
-//set up Handlebars
-Handlebars.templates = Handlebars.templates || {};
-const templates = document.querySelectorAll('template');
-Array.prototype.slice.call(templates).forEach(function(tmpl) {
-    Handlebars.templates[tmpl.id] = Handlebars.compile(tmpl.innerHTML.replace(/{{&gt;/g, '{{>'));
-});
-
-//remove previous event handlers on current view before mounting a new one
-const oldSetElement = Backbone.View.prototype.setElement;
-Backbone.View.prototype.setElement = function(el){
-  $(el).off();
-  oldSetElement.call(this,el);
-};
-
-
-
-
-
-
-
-
-//create Router
 const Router = Backbone.Router.extend({
   routes: {
     '': 'home',
@@ -76,6 +71,6 @@ const Router = Backbone.Router.extend({
   }
 });
 
-//instantiate Router and start watching for url changes
+//start listening for url changes
 const router = new Router();
 Backbone.history.start();
